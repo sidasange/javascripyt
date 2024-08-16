56c995e6501faf3a1e215d39326c27d879b5f623
sidasange <7356kamsi@gmail.com>
2024-08-16 13:16:52 +0530

    Calculator with function completed

--- Calculator/script.js
+++ Calculator/script.js
@@ -3,3 +3,12 @@ function bclick(val)
     console.log(val)
     document.getElementById("screen").value+=val
 }
+function clearDisplay(){
+    document.getElementById("screen").value=""
+}
+function result(){
+    var text=document.getElementById("screen").value
+    var result =eval(text)
+    //console.log(result)
+    document.getElementById("screen").value=result
+}
